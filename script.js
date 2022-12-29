8662a851e53cf1f6e018d3232e6b0f478c58cbd8
Nayquo <greg.2503@live.fr>
2022-12-29 23:54:40 +0100

    open and close navbar menu

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const hamburgerButton = document.querySelector(".hamburgerButton")
 const topBar = document.querySelector(".top-bar")
 const middleBar = document.querySelector(".middle-bar")
 const bottomBar = document.querySelector(".bottom-bar")
+const blackScreen = document.querySelector(".blackScreen")
+const blackScreenBlur = document.querySelector(".blackScreenBlur")
+
+
 circleText.innerHTML = circleText.textContent.replace(/\S/g,"<span class=\"spanjs\">$&</span>");
 const ele = document.querySelectorAll(".spanjs");
 for(var i = 1;i<ele.length;i++){
@@ -11,12 +15,11 @@ for(var i = 1;i<ele.length;i++){
 }
 
 
-hamburgerButton.addEventListener('click', open);
+hamburgerButton.addEventListener('click', openAndClose);
 
 let a = 0
 
-function open () {
-    console.log(a)
+function openAndClose () {
     if (a == 0){
         topBar.style.width = "20px"
         middleBar.style.width = "0"
@@ -27,6 +30,11 @@ function open () {
         bottomBar.style.left = "30%"
         topBar.style.top = "49%"
         bottomBar.style.top = "10px"
+        blackScreen.style.height = "90vh"
+        blackScreen.style.width = "90vw"
+        blackScreenBlur.style.height = "90vh"
+        blackScreenBlur.style.width = "90vw"
+        blackScreenBlur.style.opacity = "1"
         a = 1
     } else {
         topBar.style.width = "10px"
@@ -40,18 +48,11 @@ function open () {
         bottomBar.style.top = "33%"
         topBar.style.transform = "translateX(-50%)"
         bottomBar.style.transform = "translateX(-50%)"
+        blackScreen.style.height = "0vh"
+        blackScreen.style.width = "0vw"
+        blackScreenBlur.style.height = "0vh"
+        blackScreenBlur.style.width = "0vw"
+        blackScreenBlur.style.opacity = "0"
         a = 0
     }
 }
-
-// function close () {
-//     topBar.style.width = "10px"
-//     middleBar.style.width = "15px"
-//     bottomBar.style.width = "10px"
-//     topBar.style.transform = "rotate(0)"
-//     bottomBar.style.transform = "rotate(0)"
-//     topBar.style.left = "30%"
-//     bottomBar.style.left = "30%"
-//     topBar.style.top = "49%"
-//     bottomBar.style.top = "10px"
-// }
